3e4da58e2b58190102c794cd496c966779f5426b
Maksym Solomko <max.exc7@gmail.com>
2022-10-11 06:33:03 +0300

    up

--- app.js
+++ app.js
@@ -8,29 +8,34 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
   square.classList.add("square");
 
   square.addEventListener("mouseover", () => setColor(square));
-  square.addEventListener("touchstart", () => setColor(square));
+  square.addEventListener("touchstart", () => setTouchColor(square));
 
   square.addEventListener("mouseleave", () => removeColor(square));
   square.addEventListener("touchend", () => removeColor(square));
 
   board.append(square);
 }
+var touchPosition = { x: 0, y: 0 };
 
-// function setTouchColor(e) {
-//   const color = getRandomColor();
-//   // get the touch element
-//   var touch = e.touches[0];
+function setTouchColor(e) {
+  // get the touch element
+  // console.log(e.changedTouches[0]);
+  // var touch = e.changedTouches[0];
+  touchPosition.x = e.changedTouches[0].clientX - this.offsetTop;
+  touchPosition.y = e.changedTouches[0].clientY - this.offsetTop;
+  console.log(touchPosition.x);
 
-//   // get the DOM element
-//   var square = document.elementFromPoint(touch.clientX, touch.clientY);
+  // get the DOM element
+  var square = document.elementFromPoint(touch.clientX, touch.clientY);
 
-//   // make sure an element was found - some areas on the page may have no elements
-//   if (square) {
-//     // interact with the DOM element
-//     square.checked = !square.checked;
-//   } else e.style.backgroundColor = color;
-//   e.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-// }
+  // make sure an element was found - some areas on the page may have no elements
+  if (square) {
+    // interact with the DOM element
+    square.checked = !square.checked;
+  } else e.style.backgroundColor = color;
+  e.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+  const color = getRandomColor();
+}
 
 function setColor(element) {
   const color = getRandomColor();
